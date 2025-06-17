a44030725dcf819d0ccff06368c96efe624ca30d
nuta555 <nuritakaithat92@gmail.com>
2025-06-17 08:51:15 +0000

    addwhatsyourname.py

---
+++ cell01/ex03/whatsyourname.py
@@ -0,0 +1,3 @@
+frist_name = input ("Hey, what's your frist name? : nurita")
+last_name = input ("And your last name? : kaithat")
+print ("Well, pleased to meet you ,nurita kaithat", frist_name, last_name)
