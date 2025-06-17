2cbc02d712f03cec32a1c37053b63f12323f3a2e
nuta555 <nuritakaithat92@gmail.com>
2025-06-17 13:21:20 +0000

    Add password.py

--- cell02/ex00/is_zero.py
+++ cell02/ex00/is_zero.py
@@ -3,6 +3,3 @@ if value == 0:
     print("This number is equal to zero")
 else:
     print("This number is different from zero")
-    
-
-    
---
+++ cell02/ex02/password.py
@@ -0,0 +1,6 @@
+password = "666 wifi passworld"
+prompt = input()
+if prompt == password:
+    print("ACCESS GRANTED")
+else:
+    print("ACCESS DENIED")
